afbf46d92f32e2fba36d06fb519672ec8b04f677
hwalerij <valeragrul@gmail.com>
2019-11-26 22:07:12 +0100

    Use size_type instead of unsigned int for indexing

--- heap.h
+++ heap.h
@@ -5,9 +5,10 @@ template<typename KeyType, typename ValueType>
 class Heap4 {
   public:
     using KeyValueType = std::pair<KeyType, ValueType>;
+	using size_type = typename std::vector<KeyValueType>::size_type;
 
 	bool empty() const noexcept {
-		return (this->heap).empty();
+		return this->heap.empty();
 	}
 
 	void insert(KeyType const &key, ValueType const &value) {
@@ -15,28 +16,28 @@ class Heap4 {
 	}
 
 	void insert(KeyValueType const &key_value) {		
-		(this->heap).push_back(key_value);
-		this->heapifyUp((this->heap).size() - 1);
+		this->heap.push_back(key_value);
+		this->heapifyUp(this->heap.size() - 1);
 	}
 
 	KeyValueType const &peek() const {
 		if (this->heap.empty()) {
 			throw std::logic_error("The heap is empty");
 		}
-		return (this->heap)[0];
+		return this->heap[0];
 	}
 
-	void swap(const unsigned int i1, const unsigned int i2) {
-		KeyValueType temp = (this->heap)[i1];
-		(this->heap)[i1] = (this->heap)[i2];
-		(this->heap)[i2] = temp;
+	void swap(const size_type i1, const size_type i2) {
+		KeyValueType temp = this->heap[i1];
+		this->heap[i1] = this->heap[i2];
+		this->heap[i2] = temp;
 	}
 
 	KeyValueType pop() {
 		if (this->empty()) {
 			throw std::logic_error("The heap is empty");
 		}
-		KeyValueType deletedEl = (this->heap)[0];
+		KeyValueType deletedEl = this->heap[0];
 		this->swap(0, this->size() - 1);
 		this->heap.pop_back();
 		this->heapifyDown(0);
@@ -59,50 +60,58 @@ class Heap4 {
 	std::vector<KeyValueType> heap;
 
 	template<typename StreamType>
-	void printHelp(StreamType& stream, const unsigned int index, const unsigned int level) const {
+	void printHelp(StreamType& stream, const size_type index, const unsigned int level) const {
 		for (unsigned int i = 0; i < level; ++i) {
 			stream << "       ";
 		}
-		stream << (this->heap)[index].first << ": " << (this->heap)[index].second << '\n';
-		int child = -1;
-		for (unsigned int i = 1; i <= 4; ++i) {
-			child = this->child(index, i);
-			if (child > -1) {
-				this->printHelp(stream, child, level + 1);
+		stream << this->heap[index].first << ": " << this->heap[index].second << '\n';
+		size_type child = 0;
+		for (size_t i = 1; i <= 4; ++i) {
+			try {
+				child = this->child(index, i);
+			} catch(std::logic_error &ex) {
+				break;
 			}
+			this->printHelp(stream, child, level + 1);
 		}
 	}
 	
-	int child(const unsigned int index, const unsigned int childNum) const {
-		unsigned int ind = index * 4 + childNum;
-		return (ind < this->size()) ? ind : -1;
+	size_type child(const size_type index, const size_t childNum) const {
+		size_type ind = index * 4 + childNum;
+		if (ind >= this->size()) {
+			throw std::logic_error("There is no child");
+		}
+		return ind;
 	}
 
-	int parent(const unsigned int index) const {
-		return (index > 0) ? (index - 1) / 4 : -1;
+	size_type parent(const size_type index) const {
+		if (!index) {
+			throw std::logic_error("There is no parent");
+		}
+		return (index - 1) / 4;
 	}
 
-	void heapifyUp(const int index) {
-		if (index <= 0) {
+	void heapifyUp(const size_type index) {
+		if (!index) {
 			return;
 		}
-		int parentInd = -1;
-		parentInd = this->parent(index);
-		if ((this->heap)[parentInd].first < (this->heap)[index].first) {
+		size_type parentInd = this->parent(index);		
+		if (this->heap[parentInd].first < this->heap[index].first) {
 			this->swap(parentInd, index);
 			this->heapifyUp(parentInd);
 		}
 	}
 
-	void heapifyDown(const int index) {
-		int childIndex = -1;
-		unsigned int maxChildIndex = index;
-		for (int i = 1; i <= 4; ++i) {
-			childIndex = this->child(index, i);
-			if (childIndex == -1) {
+	void heapifyDown(const size_type index) {
+		size_type childIndex = 0;
+		size_type maxChildIndex = index;
+		for (size_t i = 1; i <= 4; ++i) {
+			try {
+				childIndex = this->child(index, i);
+			} catch (std::logic_error &ex) {
 				break;
 			}
-			if ((this->heap)[maxChildIndex].first < (this->heap)[childIndex].first) {
+			if (this->heap[maxChildIndex].first < this->heap[childIndex].first) {
 				maxChildIndex = childIndex;
 			}
 		}
